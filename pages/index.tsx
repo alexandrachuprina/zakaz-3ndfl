3a0d43c80be1e9668dc48df8a7857c0dcaead375
shplvk <114808997+shplvk@users.noreply.github.com>
2023-03-17 11:34:52 +0300

    Dubble text

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,15 +3,10 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 
-// import Header from 'next/';
-
-// const inter = Inter({ subsets: ['latin'] })
+import Header from '@/src/components/Header'
 
 export default function Home() {
   return (
-    
-      // <Header />
-      <></>
-    
+      <Header />    
   )
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,25 +2,14 @@ import React from 'react';
 
 import { StyledHeader } from '../../../styles/Header/Header';
 import { StyledNavbar } from '../../../styles/Header/Navbar';
+import Image from 'next/image';
 
 import Logo from '../../assets/Header/logo.svg'
-
-// export default function Header() {
-//   return (
-//     <StyledHeader>
-//       <StyledNavbar>
-//         <Logo width="200" height="200"/>
-//         <p>Hello</p>
-//       </StyledNavbar>
-//     </StyledHeader>
-//   )
-// }
 
 const Header = () => {
   return (
     <StyledHeader>
       <StyledNavbar>
-        <Logo width="200" height="200" />
         <p>Hello</p>
       </StyledNavbar>
     </StyledHeader>
