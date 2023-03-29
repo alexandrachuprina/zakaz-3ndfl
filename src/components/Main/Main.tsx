cf00b27be4d99dd282398529c191e8493c102227
shplvk <114808997+shplvk@users.noreply.github.com>
2023-03-29 16:46:31 +0300

    feat: Main fetch data LastCard component (no svg)

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -16,11 +16,17 @@ import pic07 from "../../assets/Main/pic07.svg";
 
 function Main() {
   const [myData, setMyData] = useState<any>([]);
+  const [lastCard, setLastCard] = useState<any>([]);
 
   axios.get<any>("http://localhost:1337/api/block2s").then((response) => {
     const info = response.data.data;
     // console.log(info[0].attributes)
     setMyData(info);
+  });
+  axios.get<any>("http://localhost:1337/api/block2-1s").then((response) => {
+    const info = response.data.data;
+    // console.log(info[0].attributes)
+    setLastCard(info);
   });
 
   return (
@@ -41,14 +47,21 @@ function Main() {
                 />
               ))}
             </MainWrapper>
-            {/* <MainCardLast
-              header={myData[5].attributes.header}
-              paragraph={myData[5].attributes.description}
-              //@ts-ignore
-              src={pic07}
-              buttonText={myData[5].attributes.buttontext}
-              price={false}
-            /> */}
+            {lastCard ? (
+              <>
+                {lastCard.map((item: any, i: number) => (
+                  <MainCardLast
+                    key={i}
+                    header={lastCard[i].attributes.Header}
+                    paragraph={lastCard[i].attributes.Description}
+                    //@ts-ignore
+                    src={pic01}
+                    buttonText={lastCard[i].attributes.buttontext}
+                    price={true}
+                  />
+                ))}
+              </>
+            ) : null}
           </>
         ) : null}
       </Col>
